5270c79e24057ea8613ab2b1b90bedbf949e1637
David Banks <dave@hoglet.com>
2016-01-17 19:53:04 +0000

    Made tube base address configurable
    Change-Id: I18c5249ba65744433fc3f95cb4a90b4ffc67d5d2

--- top_EMMFS.asm
+++ top_EMMFS.asm
@@ -4,6 +4,7 @@ _ROMS_=TRUE             ; Include *ROMS command (i.e. No DFS or 8271 DFS)
 _UTILS_=TRUE            ; Include utilites (*DUMP etc.) (i.e. No DFS)
 _TUBEHOST_=TRUE         ; Include Tube Host (i.e. no DFS or DFS 0.90)
 _VIA_BASE=&FCB0         ; Base Address of 6522 VIA
+_TUBE_BASE=&FCE0        ; Base Address of Tube
 
 INCLUDE "mmfs100.asm"
 
--- top_MAMMFS.asm
+++ top_MAMMFS.asm
@@ -4,6 +4,7 @@ _ROMS_=FALSE            ; Include *ROMS command (i.e. No DFS or 8271 DFS)
 _UTILS_=FALSE           ; Include utilites (*DUMP etc.) (i.e. No DFS)
 _TUBEHOST_=FALSE        ; Include Tube Host (i.e. no DFS or DFS 0.90)
 _VIA_BASE=&FE60         ; Base Address of 6522 VIA
+_TUBE_BASE=&FEE0        ; Base Address of Tube
 
 INCLUDE "mmfs100.asm"
 
