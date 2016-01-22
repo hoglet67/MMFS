ef644f6a2d71733a65370b4353d3430efefbd9a4
David Banks <dave@hoglet.com>
2016-01-22 16:29:32 +0000

    MMFS 1.13 - Optimized data transfer speed in SR Mode 2
    Change-Id: I72c938ff0fe57838dad28e923b2ea99e90bda1a0

--- MMC_UserPort.asm
+++ MMC_UserPort.asm
@@ -24,49 +24,49 @@ ELSE
 ENDIF
 
 
-	\\ Reset the User VIA
+    \\ Reset the User VIA
 .MMC_DEVICE_RESET
-	LDA #(0 + msbits)
-	STA iorb%
-	LDA #ddrmask
-	STA ddrb%
-	JSR ShiftRegMode0
-	LDA #&1C
-	STA ier%
-	RTS
+    LDA #(0 + msbits)
+    STA iorb%
+    LDA #ddrmask
+    STA ddrb%
+    JSR ShiftRegMode0
+    LDA #&1C
+    STA ier%
+    RTS
 
 
-	\\ Read byte (User Port)
-	\\ Write FF
+    \\ Read byte (User Port)
+    \\ Write FF
 .MMC_GetByte
-	LDX #(1 + msbits)
+    LDX #(1 + msbits)
 .UP_ReadByteX
-	LDA #(3 + msbits)
-	STX iorb%			;\0
-	STA iorb%
+    LDA #(3 + msbits)
+    STX iorb%           ;\0
+    STA iorb%
 .UP_ReadBits7
-	STX iorb%			;\1
-	STA iorb%
-	STX iorb%			;\2
-	STA iorb%
-	STX iorb%			;\3
-	STA iorb%
+    STX iorb%           ;\1
+    STA iorb%
+    STX iorb%           ;\2
+    STA iorb%
+    STX iorb%           ;\3
+    STA iorb%
 .UP_ReadBits4
-	STX iorb%			;\4
-	STA iorb%
-	STX iorb%			;\5
-	STA iorb%
-	STX iorb%			;\6
-	STA iorb%
-	STX iorb%			;\7
-	STA iorb%
-	LDA sr%
-	RTS
+    STX iorb%           ;\4
+    STA iorb%
+    STX iorb%           ;\5
+    STA iorb%
+    STX iorb%           ;\6
+    STA iorb%
+    STX iorb%           ;\7
+    STA iorb%
+    LDA sr%
+    RTS
 
-	\\ Write byte (User Port)
-	\\ Ignore byte in
+    \\ Write byte (User Port)
+    \\ Ignore byte in
 .UP_WriteByte
-	ASL A
+    ASL A
 IF _TURBOMMC
     STA temp
 ENDIF
@@ -79,226 +79,373 @@ IF _TURBOMMC
 ELSE
     ROL A
 ENDIF
-	AND #msmask
-	STA iorb%
-	ORA #(2 + msbits)
-	STA iorb%
+    AND #msmask
+    STA iorb%
+    ORA #(2 + msbits)
+    STA iorb%
 NEXT
-	RTS
+    RTS
 
-	\\ *** Send &FF to MMC Y times ***
-	\\ Y=0=256
+    \\ *** Send &FF to MMC Y times ***
+    \\ Y=0=256
 .MMC_16Clocks
-	LDY #2
+    LDY #2
 .MMC_Clocks
 
 {
-	LDX #(1 + msbits)
+    LDX #(1 + msbits)
 .clku1
-	JSR UP_ReadByteX		; Writes &FF
-	DEY
-	BNE clku1
-	RTS				; A=SR, X=1, Y=0
+    JSR UP_ReadByteX        ; Writes &FF
+    DEY
+    BNE clku1
+    RTS             ; A=SR, X=1, Y=0
 }
 
 
-	\\ *** Send command to MMC ***
-	\\ On exit A=result, Z=result=0
+    \\ *** Send command to MMC ***
+    \\ On exit A=result, Z=result=0
 .MMC_DoCommand
-	LDX #0
+    LDX #0
 
 {
-	LDY #7
+    LDY #7
 .dcmdu1
-	LDA cmdseq%,X
-	JSR UP_WriteByte
-	INX
-	DEY
-	BNE dcmdu1
-	JSR waitresp_up
-	JMP UP_ReadBits7
+    LDA cmdseq%,X
+    JSR UP_WriteByte
+    INX
+    DEY
+    BNE dcmdu1
+    JSR waitresp_up
+    JMP UP_ReadBits7
 }
 
-	\\ wait for response bit
-	\\ ie for clear bit (User Port only)
+    \\ wait for response bit
+    \\ ie for clear bit (User Port only)
 .waitresp_up
 {
-	LDY #0
+    LDY #0
 .wrup
-	DEY
-	BEQ wrup_timeout
-	LDX #(1 + msbits)
-	LDA #(3 + msbits)
-	STX iorb%
-	STA iorb%
-	LDA sr%
-	AND #1
-	BNE wrup
-	LDX #(1 + msbits)
-	LDA #(3 + msbits)
+    DEY
+    BEQ wrup_timeout
+    LDX #(1 + msbits)
+    LDA #(3 + msbits)
+    STX iorb%
+    STA iorb%
+    LDA sr%
+    AND #1
+    BNE wrup
+    LDX #(1 + msbits)
+    LDA #(3 + msbits)
 .wrup_timeout
-	RTS
+    RTS
 }
 
 
-	\\ *** Wait for data token ***
+    \\ *** Wait for data token ***
 .MMC_WaitForData
 {
 
-	LDX #(1 + msbits)
+    LDX #(1 + msbits)
 .wlu1
-	JSR UP_ReadByteX
-	CMP #&FE
-	BNE wlu1
-	RTS
+    JSR UP_ReadByteX
+    CMP #&FE
+    BNE wlu1
+    RTS
 }
 
-	\\ The read code below now operates in turbo mode on all hardware
-	\\ using shift register mode 2.
+    \\ The read code below now operates in turbo mode on all hardware
+    \\ using shift register mode 2.
 
-	\\ *** Read 256 bytes to datptr ***
+    \\ *** Read 256 bytes to datptr ***
 .MMC_Read256
-	LDX #0
-	BEQ MMC_ReadX
+    LDX #0
+    BEQ MMC_ReadX
 
-	\\ *** Read "byteslastsector" bytes
-	\\ to datptr ***
+    \\ *** Read "byteslastsector" bytes
+    \\ to datptr ***
 .MMC_ReadBLS
-	LDX byteslastsec%
+    LDX byteslastsec%
 
 .MMC_ReadX
-	JSR ShiftRegMode2
-	LDA TubeNoTransferIf0
-	BNE MMC_ReadToTube
+    JSR ShiftRegMode2
+    LDA TubeNoTransferIf0
+    BNE MMC_ReadToTube
 
 .MMC_ReadToMemory
-	JSR WaitForShiftDone
-	STA (datptr%),Y
-	INY
-	DEX
-	BNE MMC_ReadToMemory
-	RTS
+    JSR WaitForShiftDone
+    STA (datptr%),Y
+    INY
+    DEX
+    BNE MMC_ReadToMemory
+    RTS
 
 .MMC_ReadToTube
-	JSR WaitForShiftDone
-	STA TUBE_R3_DATA
-	DEX
-	BNE MMC_ReadToTube
-	RTS
+    JSR WaitForShiftDone
+    STA TUBE_R3_DATA
+    DEX
+    BNE MMC_ReadToTube
+    RTS
 
 
-	\\ **** Read 256 bytes to buffer ****
+    \\ **** Read 256 bytes to buffer ****
 .MMC_ReadBuffer
-	LDX #&FF
-	STX CurrentCat
-	INX
+    LDX #&FF
+    STX CurrentCat
+    INX
 
-	JSR ShiftRegMode2
+    JSR ShiftRegMode2
 
 .rdbuf2
-	JSR WaitForShiftDone
-	STA buf%, Y
-	INY
-	DEX
-	BNE rdbuf2
-	RTS
+    JSR WaitForShiftDone
+    STA buf%, Y
+    INY
+    DEX
+    BNE rdbuf2
+    RTS
+
+\\ Wait for the shift reg to complete shifing, and return the value in A.
+\\
+\\ If this is the last byte, return to mode 0 before reading the shift reg.
+\\
+\\ This could be coded in fewer instructions, but it's done this way to minimise
+\\ the time between testing for the interrupt flag, and reading the shift reg
+\\ After reading the shift reg, the next byte will be ready 16us later
+\\ Which gives us ~32 "free" instruction cycles
+\\
+\\  Here's the common path, starting with the read of the shift reg
+\\  LDA sr%             (A)    0
+\\  RTS                     6  6
+\\  STA (datptr%),Y         6 12
+\\  INY                     2 14
+\\  DEX                     2 16
+\\  BNE MMC_ReadToMemory    3 19
+\\.MMC_ReadToMemory
+\\  JSR WaitForShiftDone    6 25
+\\  LDA #4                  2 27
+\\  CPX #1                  2 29
+\\  BEQ lastByte            2 31
+\\.notLastByte
+\\  BIT ifr%            (B) 4 35 IFR should set again just in time
+\\  BEQ notLastByte         2 37
+\\  LDA sr%             (A) 4 41
+\\
+\\  Note: the above does not account for 1MHz slow down on (A,B)
+\\  because it's hard to tell empirically these add 1 or 2 cycles.
+\\
+\\  It turns out the (A) adds 1 cycle, and (B) adds 2 cycles, giving
+\\  total of 44 cycles, or 22us per byte.
+\\
+\\  I tried optmising this further, by replacing:
+\\    BIT ifr%
+\\    BEQ notLastByte
+\\  with NOPs.
+\\
+\\  with 3 NOPs, data transfer was reliable, and the code took 21us/byte:
+\\
+\\  LDA sr%             (A)    0
+\\  RTS                     6  6
+\\  STA (datptr%),Y         6 12
+\\  INY                     2 14
+\\  DEX                     2 16
+\\  BNE MMC_ReadToMemory    3 19
+\\.MMC_ReadToMemory
+\\  JSR WaitForShiftDone    6 25
+\\  LDA #4                  2 27
+\\  CPX #1                  2 29
+\\  BEQ lastByte            2 31
+\\.notLastByte
+\\  NOP                     2 33
+\\  NOP                     2 35
+\\  NOP                     2 37
+\\  LDA sr%             (A) 4 41
+\\
+\\ In this situation (A) is stretched for 1 cycle, giving 42 cycles total.
+\\
+\\  with 2 NOPs, and data transfer still reliable
+\\
+\\  LDA sr%             (A)    0
+\\  RTS                     6  6
+\\  STA (datptr%),Y         6 12
+\\  INY                     2 14
+\\  DEX                     2 16
+\\  BNE MMC_ReadToMemory    3 19
+\\.MMC_ReadToMemory
+\\  JSR WaitForShiftDone    6 25
+\\  LDA #4                  2 27
+\\  CPX #1                  2 29
+\\  BEQ lastByte            2 31
+\\.notLastByte
+\\  NOP                     2 33
+\\  NOP                     2 35
+\\  LDA sr%             (A) 4 39
+\\
+\\ In this situation (A) is stretched for 1 cycle, giving 40 cycles total.
+\\
+\\  with 1 NOPs, and data transfer still reliable
+\\
+\\  LDA sr%             (A)    0
+\\  RTS                     6  6
+\\  STA (datptr%),Y         6 12
+\\  INY                     2 14
+\\  DEX                     2 16
+\\  BNE MMC_ReadToMemory    3 19
+\\.MMC_ReadToMemory
+\\  JSR WaitForShiftDone    6 25
+\\  LDA #4                  2 27
+\\  CPX #1                  2 29
+\\  BEQ lastByte            2 31
+\\.notLastByte
+\\  NOP                     2 33
+\\  LDA sr%             (A) 4 37
+\\
+\\ In this situation (A) is stretched for 1 cycle, giving 38 cycles total.
+\\
+\\  with 0 NOPs, data transfer is unreliable, as every other byte is skipped
+\\
+\\  LDA sr%             (A)    0
+\\  RTS                     6  6
+\\  STA (datptr%),Y         6 12
+\\  INY                     2 14
+\\  DEX                     2 16
+\\  BNE MMC_ReadToMemory    3 19
+\\.MMC_ReadToMemory
+\\  JSR WaitForShiftDone    6 25
+\\  LDA #4                  2 27
+\\  CPX #1                  2 29
+\\  BEQ lastByte            2 31
+\\.notLastByte
+\\  LDA sr%             (A) 4 35
+\\
+\\ In this situation (A) is stretched for 1 cycle, giving 36 cycles total.
+\\
+\\ I tried adding just one cycle back into the main loop:
+\\
+\\  LDA sr%             (A)    0
+\\  RTS                     6  6
+\\  STA (datptr%),Y         6 12
+\\  INY                     2 14
+\\  DEX                     2 16
+\\  BNE MMC_ReadToMemory    3 19
+\\.MMC_ReadToMemory
+\\  JSR WaitForShiftDone    6 25
+\\  LDA #4                  2 27
+\\  CPX #1                  2 29
+\\  BNE notLastByte         3 32
+\\.notLastByte
+\\  LDA sr%             (A) 4 36
+\\
+\\ In this situation (A) is stretched for 2 cycle, again giving 38 cycles total, which is the same as one NOP.
+\\
+\\ So, the limit of the 6522 in the Beeb in SR Mode 2 is 19us/byte.
+\\
+\\ There is other overhead, between blocks, and interrupts.
+\\
+\\ 19us/byte for &7000 bytes actually took 680ms. This is excactly the value SWEH measured with the TurboMMC ROM.
+\\
+\\ I'm goting to return to the code as the top of this thread, as I don't like doing things by dead reconning.
+\\
+\\ 22us/byte for &7000 bytes actually took 770ms.
 
 .WaitForShiftDone
 {
-	LDA #4
-.loop
-	BIT ifr%	 \\ wait for the SR interrupt flag to be set
-	BEQ loop
-	CPX #1	   \\ if the last byte, then return to mode zero, so we don't shift an extra byte
-	BNE notLastByte
-	JSR ShiftRegMode0
-.notLastByte		
-	LDA sr%	  \\ read the data byte, and clear the SR interrupt flag
-	RTS
-}		
-		
+    LDA #4            \\ Bit 2 of IFR is the Shift Reg Interrupt flag
+    CPX #1            \\ test if the last byte
+    BEQ lastByte      \\ so we can return to mode zero before reading it
+.notLastByte
+    BIT ifr%          \\ wait for the SR interrupt flag to be set
+    BEQ notLastByte
+    LDA sr%           \\ read the data byte, and clear the SR interrupt flag
+    RTS
+.lastByte
+    BIT ifr%          \\ wait for the SR interrupt flag to be set
+    BEQ lastByte
+    JSR ShiftRegMode0 \\ returning to mode 0 here avoids an addional byte read
+    LDA sr%           \\ read the data byte, and clear the SR interrupt flag
+    RTS
+}
+
 .ShiftRegMode0
-	LDA acr%   \\ Set SR Mode to mode 0
-	AND #&E3   \\ 11100011 = SR Mode 0  
-	STA acr%   \\ CB1 is now an input
-	LDA ddrb%  \\ Set PB1 to being an output
-	ORA #&02   \\ 00000010
-	STA ddrb%
-	RTS
+    LDA acr%   \\ Set SR Mode to mode 0
+    AND #&E3   \\ 11100011 = SR Mode 0
+    STA acr%   \\ CB1 is now an input
+    LDA ddrb%  \\ Set PB1 to being an output
+    ORA #&02   \\ 00000010
+    STA ddrb%
+    RTS
 
 .ShiftRegMode2
-	LDA ddrb%  \\ Set PB1 to being an input
-	AND #&FD   \\ 11111101 
-	STA ddrb%
-	LDA acr%
-	AND #&E3   \\ 11100011
-	ORA #&08   \\ 00001000 = SR Mode 2
-	STA acr%
-	LDA sr%	   \\ Start the first read
-	LDY #0	 \\ Set the memory index to zero
-	RTS
-		
-	\\ **** Send Data Token to card ****
+    LDA ddrb%  \\ Set PB1 to being an input
+    AND #&FD   \\ 11111101
+    STA ddrb%
+    LDA acr%
+    AND #&E3   \\ 11100011
+    ORA #&08   \\ 00001000 = SR Mode 2
+    STA acr%
+    LDA sr%    \\ Start the first read
+    LDY #0   \\ Set the memory index to zero
+    RTS
+
+    \\ **** Send Data Token to card ****
 .MMC_SendingData
 {
-	LDY #2
-	JSR MMC_Clocks
-	LDA #&FE
-	JMP UP_WriteByte
+    LDY #2
+    JSR MMC_Clocks
+    LDA #&FE
+    JMP UP_WriteByte
 }
 
-	\\ **** Complete Write Operation *****
+    \\ **** Complete Write Operation *****
 .MMC_EndWrite
 {
-	LDY #2
+    LDY #2
 
-	JSR MMC_Clocks
-	JSR waitresp_up
-	JSR UP_ReadBits4
-	TAY
-	AND #&1F
-	CMP #5
-	BNE errWrite2
+    JSR MMC_Clocks
+    JSR waitresp_up
+    JSR UP_ReadBits4
+    TAY
+    AND #&1F
+    CMP #5
+    BNE errWrite2
 
-	LDX #(1 + msbits)
+    LDX #(1 + msbits)
 .ewu2
-	JSR UP_ReadByteX
-	CMP #&FF
-	BNE ewu2
-	RTS
+    JSR UP_ReadByteX
+    CMP #&FF
+    BNE ewu2
+    RTS
 }
 
-	\\ **** Write 256 bytes from dataptr% ****
+    \\ **** Write 256 bytes from dataptr% ****
 .MMC_Write256
 {
-	LDY TubeNoTransferIf0
-	BNE wruT1
+    LDY TubeNoTransferIf0
+    BNE wruT1
 
 .wru1
-	LDA (datptr%),Y
-	JSR UP_WriteByte
-	INY
-	BNE wru1
-	RTS
+    LDA (datptr%),Y
+    JSR UP_WriteByte
+    INY
+    BNE wru1
+    RTS
 
 .wruT1
-	LDY #0
+    LDY #0
 .wruT2
-	LDA TUBE_R3_DATA
-	JSR UP_WriteByte
-	INY
-	BNE wruT2
-	RTS
+    LDA TUBE_R3_DATA
+    JSR UP_WriteByte
+    INY
+    BNE wruT2
+    RTS
 }
 
-	\\ **** Write 256 bytes from buffer ****
+    \\ **** Write 256 bytes from buffer ****
 .MMC_WriteBuffer
 {
-	LDY #0
+    LDY #0
 .wbu1
-	LDA buf%,Y
-	JSR UP_WriteByte
-	INY
-	BNE wbu1
-	RTS
+    LDA buf%,Y
+    JSR UP_WriteByte
+    INY
+    BNE wbu1
+    RTS
 }
