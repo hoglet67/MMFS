d41e402843aa209455fe5f515b6a8156715bc389
David Banks <dave@hoglet.com>
2016-01-24 14:57:46 +0000

    MMFS 1.18 - Updated version number to 1.18, and add some missing files
    Change-Id: I20bd812cf26c3a07d1072f31a8a57ff20ed0bad0

---
+++ top_SWMMFST.asm
@@ -0,0 +1,16 @@
+_MASTER_=FALSE          ; Master version
+_SWRAM_=TRUE            ; Sideways RAM Version
+_ROMS_=FALSE            ; Include *ROMS command (i.e. No DFS or 8271 DFS)
+_UTILS_=TRUE            ; Include utilites (*DUMP etc.) (i.e. No DFS)
+_TUBEHOST_=TRUE         ; Include Tube Host (i.e. no DFS or DFS 0.90)
+_VIA_BASE=&FE60         ; Base Address of 6522 VIA
+_TUBE_BASE=&FEE0        ; Base Address of Tube
+_TURBOMMC=1             ; 1 = build for TurboMMC, and enable PB2-4 as outputs
+
+MACRO BASE_NAME
+	EQUS "Beeb MMFS SWRAM Turbo"
+ENDMACRO
+
+INCLUDE "mmfs100.asm"
+
+SAVE "build/SWMMFST", &8000, &C000
