b5aa18dd0fb9934d4eb1b1a1e133a5cc2cc5c593
David Banks <dave@hoglet.com>
2016-02-02 20:20:58 +0000

    Added a 2nd Master version with the VIA at FE80 (the model port)
    Change-Id: I29657a6afd641d396b71b7849cc9522c0b700ef9

---
+++ top_MAMMFS2.asm
@@ -0,0 +1,17 @@
+_MASTER_=TRUE           ; Master version
+_SWRAM_=FALSE           ; Sideways RAM Version
+_ROMS_=FALSE            ; Include *ROMS command (i.e. No DFS or 8271 DFS)
+_UTILS_=FALSE           ; Include utilites (*DUMP etc.) (i.e. No DFS)
+_TUBEHOST_=FALSE        ; Include Tube Host (i.e. no DFS or DFS 0.90)
+_VIA_BASE=&FE80         ; Base Address of 6522 VIA
+_TUBE_BASE=&FEE0        ; Base Address of Tube
+_TURBOMMC=0             ; 1 = build for TurboMMC, and enable PB2-4 as outputs
+
+MACRO BASE_NAME
+	EQUS "Master MMFS (FE80)"
+ENDMACRO
+
+INCLUDE "mmfs100.asm"
+
+SAVE "build/MAMMFS2", &8000, &C000
+        
