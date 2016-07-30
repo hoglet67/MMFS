de4a0158aabaceda69473c9e6341c21b5d57fadf
David Banks <dave@hoglet.com>
2016-07-30 13:57:35 +0100

    Added build for Master VIA at &FEA0
    Change-Id: I6dfc54e81e4ff6456dc09fb6634b4bc0d29fa776

---
+++ top_MAMMFS3.asm
@@ -0,0 +1,16 @@
+_MASTER_=TRUE           ; Master version
+_SWRAM_=FALSE           ; Sideways RAM Version
+_ROMS_=FALSE            ; Include *ROMS command (i.e. No DFS or 8271 DFS)
+_UTILS_=FALSE           ; Include utilites (*DUMP etc.) (i.e. No DFS)
+_TUBEHOST_=FALSE        ; Include Tube Host (i.e. no DFS or DFS 0.90)
+_VIA_BASE=&FEA0         ; Base Address of 6522 VIA
+_TUBE_BASE=&FEE0        ; Base Address of Tube
+_TURBOMMC=FALSE         ; true = build for TurboMMC, and enable PB2-4 as outputs
+_LARGEFILES=TRUE        ; true = enable long (>64K) file support
+_DEBUG=FALSE            ; true = enable debugging of service calls, etc
+
+MACRO BASE_NAME
+	EQUS "Master MMFS"
+ENDMACRO
+
+INCLUDE "mmfs100.asm"
