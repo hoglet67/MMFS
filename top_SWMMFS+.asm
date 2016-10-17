fef3b2aaa6deff378ba8e23d35296bbb6dc33938
Steven Flintham <sgf@lemma.co.uk>
2016-10-17 22:52:52 +0100

    Add previously uncommitted top_SWMMFS+.asm
    I just turned _ROMS_=TRUE on; it works, Acheton works.

---
+++ top_SWMMFS+.asm
@@ -0,0 +1,18 @@
+_MASTER_=FALSE          ; Master version
+_ELECTRON_=FALSE        ; Electron version
+_SWRAM_=TRUE            ; Sideways RAM Version (NB: this and B+12K both TRUE)
+_BP12K_=TRUE            ; B+ private RAM version
+_ROMS_=TRUE             ; Include *ROMS command (i.e. No DFS or 8271 DFS)
+_UTILS_=TRUE            ; Include utilites (*DUMP etc.) (i.e. No DFS)
+_TUBEHOST_=TRUE         ; Include Tube Host (i.e. no DFS or DFS 0.90)
+_VIA_BASE=&FE60         ; Base Address of 6522 VIA
+_TUBE_BASE=&FEE0        ; Base Address of Tube
+_TURBOMMC=FALSE         ; true = build for TurboMMC, and enable PB2-4 as outputs
+_LARGEFILES=TRUE        ; true = enable long (>64K) file support
+_DEBUG=FALSE            ; true = enable debugging of service calls, etc
+
+MACRO BASE_NAME
+	EQUS "Model B+ MMFS 12K"
+ENDMACRO
+
+INCLUDE "mmfs100.asm"
