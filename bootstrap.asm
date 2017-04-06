e272e518f6cb018c3299e98df66397f8afedf68c
David Banks <dave@hoglet.com>
2017-04-06 18:26:43 +0100

    SW RAM Bootstrap: 3x faster copy
    Change-Id: Ic99da7d563d429a3e3c869a18e62c337ffc6f2f1

--- bootstrap.asm
+++ bootstrap.asm
@@ -5,15 +5,50 @@ osnewl  =       &ffe7
         org     &a0
 .cpsrc  equw    &0000
 .cpdst  equw    &0000
-.cplen  equb    &00        
+.cplen  equb    &00
 .ourrom equb    &00
 .dstrom equb    &00
-        
+
         org     &8000
+
+; The included MMFS ROM is added to &4000 bytes (&3600 code, &A00 workspace)
+; The guard is &CA00 as the workspace is allowed to "overflow"
         guard   &CA00
 
+; A fixed buffer is now used for the copying code, as it's now self modifying
+; and thus contains absolute addresses.
+
+; Page &09 (9) - Envelope, Serial output, Speech, transient command buffer
+; ========================================================================
+; &0900-&09BF RS423 output buffer
+; &09C0-&09FF Speech buffer
+; &0900-&09FF CFS/RFS BPUT sequential output buffer (nb, *not* used by SAVE)
+; &0900-&09BF ENVELOPES 5 to 16
+;
+; this area is reasonable safe to use in the reset sequence
+
+code_buffer = &0900
+
+; Page &0A (10) - Serial input, transient command buffer
+; ======================================================
+; &0A00-&0AFF Cassette/RS423 input buffer (nb: *not* used by LOAD)
+;
+; this area is reasonable safe to use in the reset sequence
+
+copy_buffer = &0a00
+
+; Macros to page in the ROM in the X register
+;
+; The Electron version is compicated by the need to
+; do a double-write if the current ROM is 0x8-0xB
+;
+; As these slots correspond to Basic and the Keyboard
+; they will not be used in the copy. So a faster version
+; of the macro is used there.
+;
+; On the Beeb both versions are the same
 IF _ELECTRON_
-        macro  page_rom_x
+        MACRO  page_rom_x
         pha
         lda    #&0F
         sta    &f4
@@ -21,23 +56,29 @@ IF _ELECTRON_
         pla
         stx    &f4
         stx    &fe05
-        endmacro
+        ENDMACRO
+        MACRO  page_rom_x_fast
+        stx    &f4
+        stx    &fe05
+        ENDMACRO
 ELSE
-        macro  page_rom_x
+        MACRO  page_rom_x
         stx    &f4
         stx    &fe30
-        endmacro        
+        ENDMACRO
+        MACRO  page_rom_x_fast
+        page_rom_x
+        ENDMACRO
 ENDIF
 
-        
 .start  equs    "MRB"
         jmp     serv
         equb    %10000010
         equb    copyr-&8000
         equb    &01
-        equs    "MMFS ROM/RAM"
+        equs    "MMFS Bootstrap"
         equb    &00
-        equs    "1.0"
+        equs    "1.1"
 .copyr  equb    &00
         equs    "(C) Martin Mathers, David Banks, Steven Fosdick"
         equb    &00
@@ -67,141 +108,18 @@ ENDIF
         sta     cpsrc+1
         lda     #cmpen-cmpst
         sta     cplen
-.common lda     #&84            ; get HIMEM
-        jsr     osbyte
-        txa
-        sec                     ; subtract the size of the copy routine.
-        sbc     cplen
-        sta     cpdst
-        tya
-        sbc     #&00
-        sta     cpdst+1
+.common
         ldy     #0
 .loop1  lda     (cpsrc),Y       ; copy the copy routine into main ram.
-        sta     (cpdst),Y
+        sta     code_buffer, Y
         iny
         cpy     cplen
         bne     loop1
-        lda     #<romst         ; now set the embedded MMFS ROM as the
-        sta     cpsrc           ; source of the copy.
-        lda     #>romst
-        sta     cpsrc+1
         lda     &f4             ; including the source ROM - this one.
         sta     ourrom
-        jmp     (cpdst)         ; points to the RAM copy of the copy routine.
+        jmp     code_buffer     ; points to the RAM copy of the copy routine.
 
-        ;;  The following is copied into RAM.  It is relocatable code
-        ;;  so there is no need to change it on the fly.
-
-        macro   csetup
-        ldx     #&0f            ; find the highest free sideways RAM slot.
-.romlp
-        cpx     ourrom          ; don't write to ourrom in case it's FLASH
-        beq     romnxt
-        
-        page_rom_x
-        lda     &8006
-        eor     #&FF
-        sta     &8006
-        cmp     &8006
-        php
-        eor     #&FF
-        sta     &8006
-        plp
-        beq     testdone
-.romnxt        
-        dex
-        bpl     romlp
-
-.testdone
-        stx     dstrom        
-        ldx     ourrom          ; page back in the source ROM
-        page_rom_x
-.wait
-        lda     &8000           ; some FLASH devices vanish for a while after being written to
-        cmp     #'M'            ; MRB is a signature for for the source ROM
-        bne     wait
-        lda     &8001
-        cmp     #'R'
-        bne     wait
-        lda     &8002
-        cmp     #'B'
-        bne     wait
-
-        ldx     ourrom          ; X=FF if no RAM found
-        bpl     gotram
-        jmp     noram
-        
-.gotram
-        lda     #&00            ; set copy destination as the start of
-        sta     cpdst           ; the sideways RAM bank.
-        lda     #&80
-        sta     cpdst+1
-        ldy     #&00
-        endmacro
-
-.copyst csetup
-.copylp ldx     ourrom
-        page_rom_x
-        lda     (cpsrc),Y
-        ldx     dstrom          ; this is the destination RAM bank.
-        page_rom_x
-        sta     (cpdst),Y
-        iny
-        bne     copylp
-        inc     cpsrc+1
-        inc     cpdst+1
-        lda     cpdst+1
-        cmp     #&b6            ; the high byte of the highest address used
-        bne     copylp          ; by the MMFS ROM in its final place.
-        lda     #&aa            ; Find the ROM info table.
-        ldx     #&00
-        ldy     #&ff
-        jsr     osbyte
-        stx     cpdst
-        sty     cpdst+1
-        lda     &8006           ; copy the MMFS ROM type into the table
-        ldy     dstrom          ; entry for the RAM copy.
-        sta     (cpdst),Y
-        pla
-        tay
-        lda     #&01            ; and dont "claim" this call - others ROMS
-        ldx     ourrom          ; restore the current ROM number in X
-        rts                     ; can claim workspace (we don't).
-.copyen
-
-.cmpst  csetup
-.cmplp  ldx     ourrom
-        page_rom_x
-        lda     (cpsrc),Y
-        ldx     dstrom          ; this is the destination RAM bank.
-        page_rom_x
-        cmp     (cpdst),Y
-        bne     cmpfai
-        iny
-        bne     cmplp
-        inc     cpsrc+1
-        inc     cpdst+1
-        lda     cpdst+1
-        cmp     #&b6            ; the high byte of the highest address used
-        bne     cmplp           ; by the MMFS ROM in its final place.
-        pla
-        tay
-        lda     #&01            ; and dont "claim" this call - others ROMS
-        ldx     ourrom          ; restore the current ROM number in X
-        rts                     ; can claim workspace (we don't).
-.cmpfai ldx     ourrom
-        page_rom_x
-        jmp     romfai
-.cmpen
-
-.romfai clc
-        tya
-        adc     cpdst
-        sta     cpdst
-        lda     cpdst+1
-        adc     #&00
-        sta     cpdst+1
+.romfai
         ldx     #&00
         lda     faimsg
 .failp  jsr     oswrch
@@ -248,4 +166,146 @@ ENDIF
 .normsg equs    "No sideways RAM found for MMFS"
         equb    &0d,&0a,&00
 
-        
+
+        ;;  The following is copied into RAM.  It is relocatable code
+        ;;  so there is no need to change it on the fly.
+
+        MACRO   csetup mode
+.base
+        ldx     #&0f            ; find the highest free sideways RAM slot.
+.romlp
+        cpx     ourrom          ; don't write to ourrom in case it's FLASH
+        beq     romnxt
+
+        page_rom_x
+        lda     &8006
+        eor     #&FF
+        sta     &8006
+        cmp     &8006
+        php
+        eor     #&FF
+        sta     &8006
+        plp
+        beq     testdone
+.romnxt
+        dex
+        bpl     romlp
+
+.testdone
+        stx     dstrom
+        ldx     ourrom          ; page back in the source ROM
+        page_rom_x
+.wait
+        lda     &8000           ; some FLASH devices vanish for a while after being written to
+        cmp     #'M'            ; MRB is a signature for for the source ROM
+        bne     wait
+        lda     &8001
+        cmp     #'R'
+        bne     wait
+        lda     &8002
+        cmp     #'B'
+        bne     wait
+
+        ldx     dstrom          ; X=FF if no RAM found
+        bpl     gotram
+        jmp     noram
+
+.gotram
+        lda     #>romst         ; set the embedded MMFS ROM as thes ource of the copy
+        sta     code_buffer + patch1 + 2 - base
+        sta     code_buffer + patch2 + 2 - base
+        lda     #&80            ; set copy destination as the start the sideways RAM bank
+        sta     code_buffer + patch3 + 2 - base
+        sta     code_buffer + patch4 + 2 - base
+.cploop
+        ldx     ourrom
+        page_rom_x_fast
+        ldy     #&7f
+.cploop1
+.patch1
+        lda     romst, y            ; 4
+        sta     copy_buffer, y      ; 5
+.patch2
+        lda     romst + &80, y      ; 4
+        sta     copy_buffer + &80,y ; 5
+        dey                         ; 2
+        bpl     cploop1             ; 3
+        ldx     dstrom
+        page_rom_x_fast
+        ldy     #&7f
+.cploop2
+        lda     copy_buffer, y      ; 4
+.patch3
+IF mode=1
+        cmp     &8000, y            ; 4
+        bne     fail1               ; 2
+ELSE
+        sta     &8000, y            ; 5
+ENDIF
+        lda     copy_buffer + &80,y ; 4
+.patch4
+IF mode=1
+        cmp     &8080, y            ; 4
+        bne     fail2               ; 2
+ELSE
+        sta     &8080, y            ; 5
+ENDIF
+        dey                         ; 2
+        bpl     cploop2             ; 3
+        inc     code_buffer + patch1 + 2 - base
+        inc     code_buffer + patch2 + 2 - base
+        inc     code_buffer + patch3 + 2 - base
+        inc     code_buffer + patch4 + 2 - base
+        lda     code_buffer + patch4 + 2 - base
+        cmp     #&b6
+        bne     cploop
+IF mode=1
+        beq     exit            ; success: exit with Z=0
+.fail2
+        tya
+        ora     #&80
+        tay
+.fail1
+        sty     cpdst
+        lda     code_buffer + patch4 + 2 - base
+        sta     cpdst + 1       ; fail: exit with Z=1
+ENDIF
+.exit
+        ENDMACRO
+
+        align   &100
+
+.copyst csetup  0
+        lda     #&aa            ; Find the ROM info table.
+        ldx     #&00
+        ldy     #&ff
+        jsr     osbyte
+        stx     cpdst
+        sty     cpdst+1
+        lda     &8006           ; copy the MMFS ROM type into the table
+        ldy     dstrom          ; entry for the RAM copy.
+        sta     (cpdst),Y
+        pla
+        tay
+        lda     #&01            ; and dont "claim" this call - others ROMS
+        ldx     ourrom          ; restore the current ROM number in X
+        rts                     ; can claim workspace (we don't).
+.copyen
+
+        align   &100
+
+.cmpst  csetup  1
+        bne     cmpfai
+        pla
+        tay
+        lda     #&01            ; and dont "claim" this call - others ROMS
+        ldx     ourrom          ; restore the current ROM number in X
+        rts                     ; can claim workspace (we don't).
+.cmpfai ldx     ourrom
+        page_rom_x
+        jmp     romfai
+.cmpen
+
+; Page align the included MMFS ROM which will follow
+; This avoids any penalties with page crossing during the copy
+        align   &100
