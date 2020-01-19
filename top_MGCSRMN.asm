cc3ba43ff47ac4c29d828266db9729de49c112a1
David Banks <dave@hoglet.com>
2020-01-19 14:51:09 +0000

    MGC: 6 builds - combinations of SR/BB and Minimal/Normal/Debug
    Change-Id: I633c75cd38e9443c286bae18f4a7d3bee7401484

--- top_MGCSRMN.asm
+++ top_MGCSRMN.asm
@@ -3,7 +3,7 @@ _ELECTRON_=TRUE         ; Electron version
 _SWRAM_=TRUE            ; Sideways RAM Version
 _BP12K_=FALSE           ; B+ private RAM version
 _ROMS_=FALSE            ; Include *ROMS command (i.e. No DFS or 8271 DFS)
-_COMMANDS_=TRUE         ; Include normal DFS commands (*COPY etc)
+_COMMANDS_=FALSE        ; Include normal DFS commands (*COPY etc)
 _UTILS_=FALSE           ; Include utilites (*DUMP etc.) (i.e. No DFS)
 _TUBEHOST_=FALSE        ; Include Tube Host (i.e. no DFS or DFS 0.90)
 _VIA_BASE=&FCB0         ; Base Address of 6522 VIA
@@ -12,10 +12,10 @@ _LARGEFILES=FALSE       ; true = enable long (>64K) file support
 _DEBUG=FALSE            ; true = enable debugging of service calls, etc
 _DEBUG_MMC=FALSE        ; true = enable debugging of MMC initialization
 _DFS_EMUL=TRUE          ; true = use DFS filesystem number + handles
-_USE_MGC_SHIFTREG=FALSE ; false = Bit Bang, true = Shift Reg
+_USE_MGC_SHIFTREG=TRUE  ; false = Bit Bang, true = Shift Reg
 
 MACRO BASE_NAME
-	EQUS "MGC MMFS SWRAM (Bit Bang)"
+	EQUS "MGC MMFS SWRAM (ShiftReg, Minimal)"
 ENDMACRO
 
 INCLUDE "mmfs100.asm"
