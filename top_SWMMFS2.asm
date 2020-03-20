98879c4aa5a7f3449aabad5a7f087066394eb34b
David Banks <dave@hoglet.com>
2020-03-20 14:34:03 +0000

    Added SWMMFS2 (&FE80) build
    Change-Id: I80476900bb94e64ac6111d2252a5a1d4a0a5c14a

---
+++ top_SWMMFS2.asm
@@ -0,0 +1,20 @@
+_MASTER_=FALSE          ; Master version
+_ELECTRON_=FALSE        ; Electron version
+_SWRAM_=TRUE            ; Sideways RAM Version
+_BP12K_=FALSE           ; B+ private RAM version
+_ROMS_=FALSE            ; Include *ROMS command (i.e. No DFS or 8271 DFS)
+_COMMANDS_=TRUE         ; Include normal DFS commands (*COPY etc)
+_UTILS_=TRUE            ; Include utilites (*DUMP etc.) (i.e. No DFS)
+_TUBEHOST_=TRUE         ; Include Tube Host (i.e. no DFS or DFS 0.90)
+_VIA_BASE=&FE80         ; Base Address of 6522 VIA
+_TUBE_BASE=&FEE0        ; Base Address of Tube
+_LARGEFILES=TRUE        ; true = enable long (>64K) file support
+_DEBUG=FALSE            ; true = enable debugging of service calls, etc
+_DEBUG_MMC=FALSE        ; true = enable debugging of MMC initialization
+_DFS_EMUL=TRUE          ; true = use DFS filesystem number + handles
+
+MACRO BASE_NAME
+	EQUS "Model B MMFS SWRAM (FE80)"
+ENDMACRO
+
+INCLUDE "mmfs100.asm"
