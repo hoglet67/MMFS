47d5011222b239d8b21468de733f4a0f2e86ee7f
David Banks <dave@hoglet.com>
2020-10-25 12:13:02 +0000

    Added README.txt to release zip
    Change-Id: Ic198eca211546a6b59993efb101b16ee1aab1473

--- release.sh
+++ release.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-./build.sh
-
 # A hack, because beebasm doesn't allow string variables
-VERSION=`grep "#VERSION#" VERSION.asm  | cut -d\" -f2 | tr . _`
+VERS=$(grep "#VERSION#" VERSION.asm  | cut -d\" -f2 | tr . _)
+DATE=$(date +"%Y%m%d_%H%M")
+
+./build.sh $*
+
+README=build/README.txt
+
+echo "              MMFS and MMFS2 Release Package"    >  ${README}
+echo "              =============================="    >> ${README}
+echo ""                                                >> ${README}
+echo "              Version:                  ${VERS}" >> ${README}
+echo "              Date:            ${DATE}"          >> ${README}
+echo ""                                                >> ${README}
+cat RELEASE.txt                                        >> ${README}
 
 mkdir -p releases
 
-release=releases/mmfs_${VERSION}_$(date +"%Y%m%d_%H%M").zip
+release=releases/mmfs_${VERS}_${DATE}.zip
 cd build
 zip -qr ../${release} *
 cd ..
