7239e5555bf850fa43a02afdd2cf4ef44ff99b46
David Banks <dave@hoglet.com>
2020-10-25 10:37:00 +0000

    Build both MMFS and MMFS2 systems
    Change-Id: I3811db589c31df3d17aaab997570038fad7392c8

--- build.sh
+++ build.sh
@@ -3,8 +3,27 @@
 rm -rf build
 mkdir -p build
 
+if [ $# -eq 0 ]
+then
+    SYSTEMS="MMFS MMFS2"
+else
+   SYSTEMS=${*^^}
+fi
+
+## Sanity check systems
+for system in $SYSTEMS
+do
+    if [ $system != "MMFS" ] && [ $system != "MMFS2" ]
+    then
+        echo "Unknown system: $system"
+        exit
+    fi
+done
+
+
+
 VERSION=`grep '#VERSION#' VERSION.asm | cut -d\" -f2`
-echo "Building MMFS $VERSION"
+echo "Building $SYSTEMS version $VERSION"
 
 # Set the BEEBASM executable for the platform
 # Let's see if the user already has one on their path
@@ -23,42 +42,60 @@ fi
 BEEBASM=${BEEBASM:-tools/beebasm/beebasm.exe}
 echo Using $BEEBASM
 
-# Device:
-# U is normal User Port VIA based
-# T is User Port connected "TurboMMC" interface
-# E is Electron Plus One Printer Port connected interface (experimental)
-# M is MemoryMapped IO based (typically &FE18, for BeebEm)
-# P is Beeb Printer Port connected Interface (experimental)
-# G is Mega Games Cartridge MKII
-for device in G U T E M P
+
+# Build the two different system:
+#   MMFS is the original system that uses a BEEB.MMB file
+#   MMFS2 is the more recent system that uses direct FAT32 access of .ssd/.dsd files
+#
+for system in $SYSTEMS
 do
-    build=build/${device}
-    mkdir -p ${build}
-    ssd=${build}/mmfs.ssd
-    rm -f ${ssd}
-    # Configure the device to be assembled
-    echo "_DEVICE_ = '"$device"'" > DEVICE.asm
-    # Create a blank SSD image
-    tools/mmb_utils/blank_ssd.pl ${ssd}
-    tools/mmb_utils/title.pl ${ssd} "MMFS $device $VERSION"
-    echo
 
-    if [ $device == "E" ]
-    then
-        filelist="top_E*.asm top_ZEMMFS.asm"
-    elif [ $device == "G" ]
-    then
-        filelist="top_MGC*.asm"
-    elif [ $device == "P" ]
-    then
-        filelist="top_MMFS*.asm top_MAMMFS*.asm top_SWMMFS*.asm top_ZMMFS.asm"
-    else
-        filelist="top_*MMFS*.asm"
-    fi
-
-    for top in $filelist
+    # Device:
+    # U is normal User Port VIA based
+    # T is User Port connected "TurboMMC" interface
+    # E is Electron Plus One Printer Port connected interface (experimental)
+    # M is MemoryMapped IO based (typically &FE18, for BeebEm)
+    # P is Beeb Printer Port connected Interface (experimental)
+    # G is Mega Games Cartridge MKII
+    for device in G U T E M P
     do
-        name=`echo ${top%.asm} | cut -c5-`
+        build=build/${device}
+        mkdir -p ${build}
+        ssd=${build}/mmfs.ssd
+        rm -f ${ssd}
+
+        # Configure the system/device to be assembled
+        echo "_DEVICE_ = '"$device"'" > DEVICE.asm
+        if [ $system == "MMFS2" ]
+        then
+           echo "_MM32_      = TRUE"  >> DEVICE.asm
+           echo "_MM32_DEBUG = FALSE" >> DEVICE.asm
+           echo "_MM32_DDUMP = TRUE"  >> DEVICE.asm
+        else
+           echo "_MM32_      = FALSE" >> DEVICE.asm
+        fi
+
+        # Create a blank SSD image
+        tools/mmb_utils/blank_ssd.pl ${ssd}
+        tools/mmb_utils/title.pl ${ssd} "MMFS $device $VERSION"
+        echo
+
+        if [ $device == "E" ]
+        then
+            filelist="top_E*.asm top_ZEMMFS.asm"
+        elif [ $device == "G" ]
+        then
+            filelist="top_MGC*.asm"
+        elif [ $device == "P" ]
+        then
+            filelist="top_MMFS*.asm top_MAMMFS*.asm top_SWMMFS*.asm top_ZMMFS.asm"
+        else
+            filelist="top_*MMFS*.asm"
+        fi
+
+        for top in $filelist
+        do
+            name=`echo ${top%.asm} | cut -c5-`
 
 #        if [ \
 #            "${device}/${name}" == "T/SWMMFS" -o \
@@ -71,42 +108,52 @@ do
             echo "Building ${device}/$name..."
 #        fi
 
-        # Assemble the ROM
-        $BEEBASM -i ${top} -o ${build}/${name} -v >& ${build}/${name}.log
+            # Assemble the ROM
+            $BEEBASM -i ${top} -o ${build}/${name} -v >& ${build}/${name}.log
 
-        # Check if ROM has been build, otherwise fail early
-        if [ ! -f ${build}/${name} ]
-        then
-            cat ${build}/${name}.log
-            echo "build failed to create ${device}/${name}"
-            exit
-        fi
+            # Check if ROM has been build, otherwise fail early
+            if [ ! -f ${build}/${name} ]
+            then
+                cat ${build}/${name}.log
+                echo "build failed to create ${device}/${name}"
+                exit
+            fi
 
-        # To save space, exclude the Z builds from the .ssd image
-        if [[ $name != Z* ]]
-        then
-            # Create the .inf file
-            echo -e "\$."${name}"\t8000\t8000" > ${build}/${name}.inf
+            # To save space, exclude the Z builds from the .ssd image
+            if [[ $name != Z* ]]
+            then
+                # Create the .inf file
+                echo -e "\$."${name}"\t8000\t8000" > ${build}/${name}.inf
 
-            # Add into the SSD
-            tools/mmb_utils/putfile.pl ${ssd} ${build}/${name}
+                # Add into the SSD
+                tools/mmb_utils/putfile.pl ${ssd} ${build}/${name}
 
-            # Delete the .inf file
-            rm -f ${build}/${name}.inf
-        fi
+                # Delete the .inf file
+                rm -f ${build}/${name}.inf
+            fi
 
-        # Report end of code
-        grep "code ends at" ${build}/${name}.log
+            # Report end of code
+            grep "code ends at" ${build}/${name}.log
 
-        # Report build checksum
-        echo "    mdsum is "`$MD5SUM <${build}/${name}`
+            # Report build checksum
+            echo "    mdsum is "`$MD5SUM <${build}/${name}`
 
-        # Add a .rom suffix
-        mv ${build}/${name} ${build}/${name}.rom
-    done
-    # Copy utilities
-    tools/mmb_utils/putfile.pl ${ssd} utilities/bin/*
-    echo
-    tools/mmb_utils/info.pl  ${ssd}
-done
-rm -f DEVICE.asm
+            # Add a .rom suffix
+            mv ${build}/${name} ${build}/${name}.rom
+        done
+        # Copy utilities
+        tools/mmb_utils/putfile.pl ${ssd} utilities/bin/*
+        echo
+        tools/mmb_utils/info.pl  ${ssd}
+
+        rm -f DEVICE.asm
+
+    done # for device in G U T E M P
+
+    # Move the files in the build directory to the right system folder
+    # It would be cleaner to build in the correct directory in the first place
+    # but ZMMFS sources contain absolute build paths so this is simpler.
+    mkdir -p build/${system}
+    mv build/[A-Z] build/${system}
+
+done # for system in mmfs mmfs2
