04158c1bae2fd01e43720d6ad2aab6aa83a9cc85
dp111 <19616418+dp111@users.noreply.github.com>
2024-05-04 16:40:37 +0100

    top_SWMMFS.asm : enable *ROMS for Pi1MHz

--- top_SWMMFS.asm
+++ top_SWMMFS.asm
@@ -4,7 +4,7 @@ _MASTER_=FALSE          ; Master version
 _ELECTRON_=FALSE        ; Electron version
 _SWRAM_=TRUE            ; Sideways RAM Version
 _BP12K_=FALSE           ; B+ private RAM version
-_ROMS_=FALSE            ; Include *ROMS command (i.e. No DFS or 8271 DFS)
+;_ROMS_=FALSE            ; Include *ROMS command (i.e. No DFS or 8271 DFS)
 _COMMANDS_=TRUE         ; Include normal DFS commands (*COPY etc)
 _UTILS_=TRUE            ; Include utilites (*DUMP etc.) (i.e. No DFS)
 _TUBEHOST_=TRUE         ; Include Tube Host (i.e. no DFS or DFS 0.90)
@@ -13,6 +13,12 @@ _LARGEFILES=TRUE        ; true = enable long (>64K) file support
 _DEBUG=FALSE            ; true = enable debugging of service calls, etc
 _DEBUG_MMC=FALSE        ; true = enable debugging of MMC initialization
 _DFS_EMUL=TRUE          ; true = use DFS filesystem number + handles
+
+IF _DEVICE_ = "1"
+    _ROMS_=TRUE
+ELSE
+    _ROMS_=FALSE
+ENDIF
 
 MACRO BASE_NAME
     EQUS "Model B "
