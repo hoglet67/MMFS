07dab905637d1efeee28fc441c4823531fa642c8
dp111 <19616418+dp111@users.noreply.github.com>
2024-05-04 20:37:55 +0100

    mmfs100.asm : combine OW7F_Execute and ReportIFDiskFault

--- mmfs100.asm
+++ mmfs100.asm
@@ -2119,8 +2119,7 @@ IF _MM32_
 	LDA &C0
 	STA OWCtlBlock+11
 
-	JSR OW7F_Execute
-	JSR ReportIfDiskFault
+	JSR OW7F_Execute_and_ReportIfDiskFault
 
 	LDA #1
 	RTS
@@ -6938,8 +6937,7 @@ IF _MM32_
 	PLA
 	STA OWCtlBlock+6
 
-	JSR OW7F_Execute
-	JSR ReportIfDiskFault
+	JSR OW7F_Execute_and_ReportIfDiskFault
 
 	LDA CurrentDrv
 	STA CurrentCat
@@ -6974,7 +6972,8 @@ IF _MM32_
 	RTS
 }
 
-
+.OW7F_Execute_and_ReportIfDiskFault
+	JSR OW7F_Execute
 \\ Report 'disk fault'
 \\ On entry: A=FDC result, Z=1 if A=0
 .ReportIfDiskFault
